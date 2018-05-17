9c3d1ea06a234e97b1d111993d2b0345123f01c1
mikk7657 <39336080+mikk7657@users.noreply.github.com>
2018-05-17 14:48:23 +0200

    Add files via upload

---
+++ Kasseapparat_H3/Kasseapparat_H3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasseapparat_H3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hvad har kunden købt?: ");
+            string vare = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Hvad koster varen?: ");
+            string pris = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine(vare);
+            Console.WriteLine(pris + " kr.");
+            
+            
+
+            Console.ReadLine();
+        }
+    }
+}
